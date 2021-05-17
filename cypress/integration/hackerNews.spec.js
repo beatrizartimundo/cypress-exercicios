0d0738b03e68a7562e2dadd1d8deb43f54ae4954
beatrizartimundo <beatriz.artimundo@gmail.com>
2021-05-17 19:23:37 -0300

    add cache test

--- cypress/integration/hackerNews.spec.js
+++ cypress/integration/hackerNews.spec.js
@@ -22,8 +22,111 @@ context('Hitting the API', () => {
 context.only('Mocking the API', () => {
   
   const term = 'cypress'
+  const stories = require('../fixtures/stories.json')
 
-  context('list of stories', () => {
+  context('list stories', () => {
+    beforeEach(() => {
+      cy.intercept(
+        `search?query=redux&page=0&hitsPerPage=100`,
+        { fixture: 'stories'}
+      ).as('reduxStories')
+  
+      cy.visit('/')
+      cy.wait('@reduxStories')
+    })
+
+    it('shows the right data for the stories', () => {
+      cy.get('.table-row')
+      .first()
+      .should('be.visible')
+      .should('contain',stories.hits[0].title)
+      .and('contain',stories.hits[0].author)
+      .and('contain',stories.hits[0].num_comments)
+      .and('contain',stories.hits[0].points)
+
+      cy.get('.table-row')
+      .last()
+      .should('be.visible')
+      .should('contain',stories.hits[1].title)
+      .and('contain',stories.hits[1].author)
+      .and('contain',stories.hits[1].num_comments)
+      .and('contain',stories.hits[1].points)
+      
+    });
+    it('shows one story after dimissing the first one', () => {
+      cy.get('.button-inline:contains(Dismiss)')
+      .first()
+      .should('be.visible')
+      .click()
+
+      cy.get('.table').should('have.length',1)
+    })
+
+    context('Order the content', () => {
+
+      it('order by title', () => {
+        cy.get('.button-inline:contains(Title)')
+        .as('title')
+        .should('be.visible')
+        .click()
+
+        cy.get('.table')
+          .first()
+          .should('be.visible')
+          .and('contain', stories.hits[0].title)
+        cy.get(`.table a:contains(${stories.hits[0].title})`)
+          .should('have.attr', 'href', stories.hits[0].url)
+        cy.get('@title').click()
+        cy.get('.table')
+        .first()
+        .should('be.visible')
+        .and('contain', stories.hits[1].title)
+      cy.get(`.table a:contains(${stories.hits[1].title})`)
+        .should('have.attr', 'href', stories.hits[1].url)
+      });
+
+      it('order by author', () => {
+        cy.get('.button-inline:contains(Author)')
+        .as('author')
+        .should('be.visible')
+        .click()
+
+        cy.get('.table')
+        .first()
+        .should('be.visible')
+        .and('contain', stories.hits[0].author)
+
+        cy.get('@author').click()
+
+        cy.get('.table')
+        .first()
+        .should('be.visible')
+        .and('contain', stories.hits[1].author)
+
+      });
+
+      it('order by points', () => {
+        cy.get('.button-inline:contains(Points)')
+        .as('point')
+        .should('be.visible')
+        .click()
+
+        cy.get('.table')
+        .first()
+        .should('be.visible')
+        .and('contain', stories.hits[0].points)
+
+        cy.get('@point').click()
+
+        cy.get('.table')
+        .first()
+        .should('be.visible')
+        .and('contain', stories.hits[1].points)
+
+      });
+    })
+  })
+  context('search stories', () => {
     beforeEach(() => {
       cy.intercept(
         '**/search?query=redux&page=0&hitsPerPage=100',
@@ -61,6 +164,24 @@ context.only('Mocking the API', () => {
           expect(count, `network calls to fetch ${randomWord}`).to.equal(1)
         })
       })
+    })
+
+    it('shows no story when none is returned', () => {
+      cy.get('.table').should('not.exist')
+    });
+
+    it('types and hits ENTER', () => {
+      cy.get('input[type="text"]')
+        .should('be.visible')
+        .type(`${term}{enter}`)
+
+      cy.wait('@stories')
+
+      cy.getLocalStorage('search')
+        .should('be.equal', term)
+
+      
+      
     })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,5 +3,6 @@ import "cypress-localstorage-commands"
 Cypress.Commands.add('search', term => {
     cy.get('input[type="text"]')
     .should('be.visible')
+    .clear()
     .type(`${term}{enter}`)
 })
